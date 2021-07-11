133f20dd97415950f220bdc0588a1c8979ce6f67
kenweechin <kenweechin@gmail.com>
2021-07-11 14:04:56 +0000

    Move google map markers code to maps.js

---
+++ assets/js/maps.js
@@ -0,0 +1,44 @@
+//rendering the map to the page
+//code is derived and edited from google maps platform
+
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 10,
+        center: {
+            lat: 53.350140,
+            lng: -6.266155
+        }
+    });
+
+    // create label with string for the markers.
+    const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    // create an array which contains objects where the coffee shops located.
+    const locations = [{
+            lat: 53.394660,
+            lng: -6.245100
+        },
+        {
+            lat: 53.403470,
+            lng: -6.343720
+        },
+        {
+            lat: 53.354300,
+            lng: -6.261570
+        },
+
+    ];
+
+    // create a new marker 
+    const markers = locations.map((location, i) => {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length],
+        });
+    });
+
+    // add a marker clusterer
+    new MarkerClusterer(map, markers, {
+        imagePath: "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+    });
+}
