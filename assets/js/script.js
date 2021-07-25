6ecf6e530201a2c2648d7f64e490f326e1515372
kenweechin <kenweechin@gmail.com>
2021-07-25 13:06:30 +0000

    Add alert method and edit alert message in script.js for index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 //Mouseover event for the Order Now button in the home page
-<<<<<<< HEAD
 document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByClassName("orderButton");
 
@@ -8,25 +7,10 @@ document.addEventListener("DOMContentLoaded", function () {
             orderButton.textContent = "Good Choice!";
         })
         orderButton.addEventListener("click", function () {
-            alert("You have added your choice to the cart!");
+            alert("Your choice has been added to the cart!");
         })
         orderButton.addEventListener("mouseout", function () {
             orderButton.textContent = "Order Now";
         })
     } 
-
-=======
-document.addEventListener("DOMContentLoaded", function(){
-    let buttons = document.getElementsByClassName("orderButton");
-
-    for (let orderButton of buttons) {
-        orderButton.addEventListener("mouseover", function(){
-            orderButton.textContent = "Good Choice!";
-        })
-
-        orderButton.addEventListener("mouseout", function(){
-            orderButton.textContent = "Order Now";
-        })
-    }
->>>>>>> 5ab37fb500262ad5111b37698ad01478a3fdfe93
 })
