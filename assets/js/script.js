da6d3224c7298d0062645304ea428bb30328b5e8
kenweechin <kenweechin@gmail.com>
2021-07-25 11:43:46 +0000

    Add alert method to script.js for index.html Order Now button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,17 @@
-document.addEventListener("DOMContentLoaded", function(){
-    let buttons = document.getElementsByTagName("button");
+//Mouseover event for the Order Now button in the home page
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByClassName("orderButton");
 
-    for (let button of buttons) {
-        button.addEventListener("mouseover", function(){
-            button.textContent = "Good Choice!";
+    for (let orderButton of buttons) {
+        orderButton.addEventListener("mouseover", function () {
+            orderButton.textContent = "Good Choice!";
         })
+        orderButton.addEventListener("click", function () {
+            alert("You have added your choice to the cart!");
+        })
+        orderButton.addEventListener("mouseout", function () {
+            orderButton.textContent = "Order Now";
+        })
+    } 
 
-        button.addEventListener("mouseout", function(){
-            button.textContent = "Order Now";
-        })
-    }
 })
