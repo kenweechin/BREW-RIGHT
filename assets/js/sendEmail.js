ff5982c1c368e0f88c9b4059687451f15b940824
kenweechin <kenweechin@gmail.com>
2021-07-25 14:24:32 +0000

    Add alert method to sendEmail.js for contact.html

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,17 +1,26 @@
 function sendMail(contactForm) {
     emailjs.send("service_44tf1cv", "brewright", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "order_request": contactForm.coffeerequest.value
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
-    );
-    return false;  // To block from loading a new page
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.emailaddress.value,
+            "order_request": contactForm.coffeerequest.value
+        })
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+            },
+            function (error) {
+                console.log("FAILED", error);
+            }
+        );
+    return false; // To block from loading a new page
 }
 
+//alert method for the contact page after user has submitted request.
+let button = document.getElementsByClassName("submitButton");
+    
+    for (let submitButton of button){
+        submitButton.addEventListener("click", function () {
+            alert("You had submitted your request. We will get back to you soon!");
+        })
+    }
+    
