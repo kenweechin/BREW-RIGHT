9e17c941788709e8fefcdc2b59f4fb0efaf8914c
kenweechin <kenweechin@gmail.com>
2021-07-27 21:34:27 +0000

    Integrate cart adding features for index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,61 @@ document.addEventListener("DOMContentLoaded", function () {
     } 
 })
 
+//Carts section
+let cart = document.querySelectorAll(".orderButton");
+
+let product = [
+    {
+        name: "FRENCH PRESS",
+        price: 7.99,
+        insideCart: 0
+    },
+    {
+        name: "IBRIK",
+        price: 8.99,
+        insideCart: 0
+    },
+    {
+        name: "MOKAPOT",
+        price: 8.99,
+        insideCart: 0
+    },
+    {
+        name: "POUROVER",
+        price: 5.99,
+        insideCart: 0
+    }
+]
+
+for (let i=0; i < cart.length; i++){
+    cart[i].addEventListener("click", function(){
+        cartQuantity() 
+    })
+}
+
+function loadedCart() {
+    let cartNumber = localStorage.getItem("cartQuantity");
+
+    if (cartNumber) 
+        document.querySelector(".cart-number").textContent = cartNumber;
+}
+
+function cartQuantity() {
+    let cartNumber = localStorage.getItem("cartQuantity");
+
+    cartNumber = parseInt(cartNumber);
+    
+    if (cartNumber) {
+        localStorage.setItem("cartQuantity", cartNumber + 1);
+        document.querySelector(".cart-number").textContent = cartNumber + 1;
+    }
+    else {
+        localStorage.setItem("cartQuantity", 1);
+        document.querySelector(".cart-number").textContent =1;
+    } 
+} 
+
+loadedCart()
 
 
   
