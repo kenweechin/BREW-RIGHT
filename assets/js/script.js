30fd014aa73b9d59c733cbf8e47ee862fb125827
kenweechin <kenweechin@gmail.com>
2021-07-30 23:41:20 +0000

    Integrate product updating list feature on index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,14 +11,13 @@ document.addEventListener("DOMContentLoaded", function () {
         orderButton.addEventListener("mouseout", function () {
             orderButton.textContent = "Order Now";
         })
-    } 
+    }
 })
 
 //Carts section
 let cart = document.querySelectorAll(".orderButton");
 
-let product = [
-    {
+let items = [{
         name: "FRENCH PRESS",
         price: 7.99,
         insideCart: 0
@@ -40,35 +39,58 @@ let product = [
     }
 ]
 
-for (let i=0; i < cart.length; i++){
-    cart[i].addEventListener("click", function(){
-        cartQuantity() 
+for (let i = 0; i < cart.length; i++) {
+    cart[i].addEventListener("click", function () {
+        cartQuantity(items[i])
     })
+}
+
+function cartQuantity(items) {
+
+    let cartNumber = localStorage.getItem("cartQuantity");
+
+    cartNumber = parseInt(cartNumber);
+
+    if (cartNumber) {
+        localStorage.setItem("cartQuantity", cartNumber + 1);
+        document.querySelector(".cart-number").innerHTML = cartNumber + 1;
+    } else {
+        localStorage.setItem("cartQuantity", 1);
+        document.querySelector(".cart-number").innerHTML = 1;
+    }
+
+    setCoffeeProducts(items);
+}
+
+function setCoffeeProducts(items) {
+    let cartProduct = localStorage.getItem('coffeeInCart');
+
+    cartProduct = JSON.parse(cartProduct);
+
+    if (cartProduct != null) {
+         if (cartProduct[items.name] == undefined) {
+             cartProduct = {
+                ...cartProduct,
+                [items.name]: items
+             }
+         }
+        cartProduct[items.name].insideCart += 1;
+    } else {
+        items.insideCart = 1;
+
+        cartProduct = {
+            [items.name]: items
+        }
+    }
+
+    localStorage.setItem("coffeeInCart", JSON.stringify(cartProduct));
 }
 
 function loadedCart() {
     let cartNumber = localStorage.getItem("cartQuantity");
 
-    if (cartNumber) 
-        document.querySelector(".cart-number").textContent = cartNumber;
+    if (cartNumber)
+        document.querySelector(".cart-number").innerHTML = cartNumber;
 }
 
-function cartQuantity() {
-    let cartNumber = localStorage.getItem("cartQuantity");
-
-    cartNumber = parseInt(cartNumber);
-    
-    if (cartNumber) {
-        localStorage.setItem("cartQuantity", cartNumber + 1);
-        document.querySelector(".cart-number").textContent = cartNumber + 1;
-    }
-    else {
-        localStorage.setItem("cartQuantity", 1);
-        document.querySelector(".cart-number").textContent =1;
-    } 
-} 
-
-loadedCart()
-
-
-  
+loadedCart();
